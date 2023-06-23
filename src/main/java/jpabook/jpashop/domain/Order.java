88639bf9ac84d902ffb847cb0a41e4df5b16a032
Yeongbb <handfaste@naver.com>
2023-06-23 17:57:50 +0900

    Finish

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -28,10 +28,10 @@ public class Order {
     @JoinColumn(name = "member_id")     // fk = member_id
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
